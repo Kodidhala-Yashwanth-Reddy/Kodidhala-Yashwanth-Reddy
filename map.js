bcf2f00a18d71ee1f3130f312b50e0b5a92233f2
Kodidhala-Yashwanth-Reddy <166115954+Kodidhala-Yashwanth-Reddy@users.noreply.github.com>
2024-04-05 14:50:54 +0530

    Add files via upload

---
+++ map.js
@@ -0,0 +1,8 @@
+"use strict";
+new google.maps.Map(document.getElementById('map'), {
+    zoom: 1,
+    center: {
+        lat: 0,
+        lng: 0
+    }
+});
---
+++ map.ts
@@ -0,0 +1,9 @@
+new google.maps.Map(document.getElementById('map')!, {
+    zoom: 1,
+    center: {
+        lat: 0,
+        lng:0
+
+    }
+
+});
